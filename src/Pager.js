c8e01218faa19b9d4f4c5fdfc4588228ae63168b
Sennka Ryuu <sennkaryuu@Sennkas-MacBook-Pro.local>
2020-05-15 18:01:04 +0800

    分页插件

--- src/Pager.js
+++ src/Pager.js
@@ -1,66 +1,141 @@
 /*
  * @Author: ryuusennka
- * @Date: 2020-04-30 13:17:02
+ * @Date: 2020-05-15 10:52:29
  * @LastEditors: ryuusennka
- * @LastEditTime: 2020-05-08 23:50:24
+ * @LastEditTime: 2020-05-15 18:00:11
  * @FilePath: /sennka-tools/src/Pager.js
- * @Description:
+ * @Description: 分页插件
  */
 
-function Pager(selector, options = { totalElements: 1000 }, cb) {
+/**
+ *
+ * @param {String} selector  如 id(#id), 类名(.class)
+ * @param {Object} options 必传
+ * @param {Number} options.totalElements 总记录数
+ * @param {Number} options.size 每页记录数
+ * @param {Number} options.links 显示多少个页码,建议填写奇数
+ * @param {Number} options.page 当前是第几页
+ * @param {Function} cb 回调函数，比如处理 ajax
+ */
+function Pager(selector, options = {}, cb) {
   if (!(this instanceof Pager)) return new Pager(selector, options, cb);
-  this.el = document.querySelector(selector);
   if (cb) this.cb = cb;
-  this.pageParamName = options.pageParamName || 'page';
-  // this.urlHash = window.location.hash.substr(1) || '';
-  if (!options.url) options.url = window.location.href;
-  this.queryStr = window.location.search.substr(1) || '';
-  this.queryObj = this.paramStrToObj(this.queryStr);
-  this.currentPage = options.currentPage || 1;
-  this.totalElements = options.totalElements * 1;
-  this.size = options.size * 1 || 10;
+  this.selector = selector;
+  this.el = document.querySelector(selector);
+  for (var k in options) {
+    this[k] = Number(options[k]);
+  }
+  this.searchParamStr = window.location.search.substr(1);
+  this.hash = window.location.hash;
+  this.searchParamObj = this.stringToObject(this.searchParamStr);
+  // 以传入的page为主，否则从url上面取page
+  if (this.page) {
+    this.searchParamObj['page'] = this.page;
+  } else if (this.searchParamObj['page']) {
+    this.page = this.searchParamObj['page'] *= 1;
+  }
+  // 计算 totalPages
   this.totalPages = Math.ceil(this.totalElements / this.size);
-  this.links = options.links * 1 || 5;
+  // 计算 links
   const leftNum = Math.floor(this.links / 2);
-  let left = Math.max(1, this.currentPage - leftNum);
+  let left = Math.max(1, this.page - leftNum);
   const right = Math.min(this.totalPages, left + this.links - 1);
   left = Math.max(1, right - this.links + 1);
   this.pageLinks = [];
   for (let i = left; i <= right; i++) {
-    // this.queryObj[this.pageParamName] = i;
-    // this.pageLinks.push(this.objToParamStr(this.queryObj));
-    this.pageLinks.push(this.queryObj.replace());
+    this.pageLinks.push({ page: i });
   }
-  console.log(this.pageLinks);
 }
 
 Pager.prototype = {
   /**
-   * 将字符串的传参转为对象形式
-   * 如："name=foo&age=24" 得到 {name:"foo",age: 24}
-   * @param {*} queryStr window.location.search.substr(1)
-   * @returns {Object}
+   * query字符串转对象
+   * @param {String} str 需要被转换成对象的字符串
    */
-  paramStrToObj: function (queryStr) {
-    var param = queryStr.split('&');
-    var obj = {};
-    for (var i = 0; i < param.length; i++) {
-      obj[param[i].split('=')[0]] = param[i].split('=')[1];
+  stringToObject(str) {
+    const param = Array.prototype.slice.call(str.split('&'));
+    const o = {};
+    for (let i = 0; i < param.length; i++) {
+      o[param[i].split('=')[0]] = param[i].split('=')[1];
     }
-    return obj;
+    return o;
   },
   /**
-   * 将对象转为字符串形式的传参
-   * 如 {name:"foo",age: 24} 得到 "name=foo&age=24"
-   * @param {Object} obj
-   * @returns {String}
+   * 对象转query字符串
+   * @param {Object} obj 需要被转换成字符串的对象
    */
-  objToParamStr: function (obj) {
-    var str = '';
+  objectToString(obj) {
+    let str = '';
     for (let k in obj) {
       str += `${k}=${obj[k]}&`;
     }
     str = str.substr(0, str.length - 1);
     return str;
   },
+  /**
+   * 保留query传参生成页码
+   * @param {Number} page 页码
+   * @return {String} 返回search和hash
+   */
+  generateLink(page) {
+    this.searchParamObj['page'] = page;
+    return '?' + this.objectToString(this.searchParamObj) + this.hash;
+  },
+  /**
+   * 输出自定义分页
+   */
+  renderPageStyle1() {
+    // -----select-----
+    let selectStr = '';
+    let optionStr = '';
+    for (let i = 1; i <= this.totalPages; i++) {
+      if (this.page === i) {
+        optionStr += `<option value="${i}" selected>第${i}页</option>`;
+      } else {
+        optionStr += `<option value="${i}">第${i}页</option>`;
+      }
+    }
+    selectStr += `<select id="topage">${optionStr}</select>`;
+
+    // -----links-----
+    let links = '';
+    // 首页
+    let firstLink = `<a href="${this.generateLink(1)}">首页</a>`;
+    links += firstLink; // 拼接首页
+    // 上一页
+    // prettier-ignore
+    let previousPage = `<a href="${this.generateLink(Math.max(1, this.page - 1))}">上一页</a>`;
+    links += previousPage; // 拼接上一页
+    // 下一页
+    // prettier-ignore
+    let nextLink = `<a href="${this.generateLink(Math.min(this.page + 1, this.totalPages))}">下一页</a>`;
+    // 尾页
+    let lastLink = `<a href="${this.generateLink(this.totalPages)}">尾页</a>`;
+    // 拼接页码
+    for (let i = 0; i < this.pageLinks.length; i++) {
+      const page = this.pageLinks[i]['page'];
+      if (page === this.page) {
+        // prettier-ignore
+        links += `<a href="${this.generateLink(page)}" class="current">${page}</a>`;
+      } else {
+        links += `<a href="${this.generateLink(page)}">${page}</a>`;
+      }
+    }
+    links += nextLink; // 拼接下一页
+    links += lastLink; // 拼接尾页
+    // 可能额外展示信息
+    let info = `<span>总记录数:<span>${this.totalElements}</span></span>`;
+    // 样式渲染
+    const style = document.createElement('style');
+    const cssText = `${this.selector} select {display: inline-block;}
+${this.selector} a {display: inline-block;border: 1px solid #ddd;padding: .2em .4em;margin: 0 .3em;}
+${this.selector} a.current {background: #999;}`;
+    style.innerHTML = cssText;
+    this.el.innerHTML = selectStr + links + info;
+    document.querySelector('head').appendChild(style);
+    // 执行回调函数
+    if (this.cb) this.cb();
+  },
 };
+
+export default Pager;
